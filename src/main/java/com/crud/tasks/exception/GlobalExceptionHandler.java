8b12098eacc86c5bd20a68fb52e193e0fc402b39
Maciej Kabaj <maciejkabaj93@gmail.com>
2025-09-03 11:44:53 +0200

    18.2 Added task retrieval by ID with exception handling and global 404 response

---
+++ src/main/java/com/crud/tasks/exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.crud.tasks.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(TaskNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleTaskNotFoundException(TaskNotFoundException ex) {
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("error", ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+}
