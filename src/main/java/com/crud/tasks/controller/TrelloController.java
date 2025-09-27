fe5149e6848c99d99ab749c268fe6eddb7ab1f94
Maciej Kabaj <maciejkabaj93@gmail.com>
2025-09-27 10:45:58 +0200

    21.2 Added Trello username property, extracted URL builder, and filtered Trello boards by ID, name, and 'Kodilla' keyword

---
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -0,0 +1,34 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.trello.client.TrelloClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("v1/trello")
+@RequiredArgsConstructor
+@CrossOrigin("*")
+public class TrelloController {
+
+    private final TrelloClient trelloClient;
+
+    @GetMapping("boards")
+    public void getTrelloBoards() {
+        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards().stream()
+                .filter(trelloBoardDto ->
+                        trelloBoardDto.getId() != null &&
+                        trelloBoardDto.getName() != null &&
+                        trelloBoardDto.getName().contains("Kodilla"))
+                .toList();
+        trelloBoards.forEach(trelloBoardDto -> {
+            System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName());
+        });
+    }
+}
